88cec77bcf260fc931a0895d9aa2e8f99433f5e1
James Smith Wigglesworth <jamesmithwigglesworth@gmail.com>
2024-05-06 15:25:26 +0700

    feat: modify import_data.py to comply with excel update

--- server/server/import_data.py
+++ server/server/import_data.py
@@ -45,6 +45,7 @@ def import_SP_curriculum(excel_file):
         
         study_program = clean_cell(study_program)
         curriculum = clean_cell(curriculum)
+        curriculum_desc = clean_cell(curriculum_desc)
 
         print(study_program, curriculum, curriculum_desc)
 
@@ -65,7 +66,7 @@ def import_SP_curriculum(excel_file):
             INSERT {{
                 :{curriculum} a :Curriculum ;
                     :curriculumName "{curriculum_desc}"
-            }}
+            }}  
             WHERE {{
                 FILTER NOT EXISTS {{
                     :{curriculum} a :Curriculum
@@ -81,7 +82,7 @@ def import_SP_curriculum(excel_file):
                 }}
             }};
             """
-
+        
         sparql = SPARQLWrapper(GRAPHDB_POST) 
         sparql.setQuery(query_string)
         sparql.setReturnFormat(JSON)
@@ -89,7 +90,10 @@ def import_SP_curriculum(excel_file):
 
         print("sparqql", query_string)
 
-        sparql.queryAndConvert()
+        try: 
+            sparql.queryAndConvert()
+        except:
+            print("ERROR")
         print("ACHIEVED")
 
     return error_list
@@ -103,18 +107,32 @@ def import_curriculum_PEO(excel_file):
     for index, row in df.iterrows():
         curriculum = str(row['Curriculum']) if str(row['Curriculum']) != "nan" else ""
         peo = str(row['PEO']) if str(row['PEO']) != "nan" else ""
-        nqf_auth_resp = str(row['nqfAuthorityResponsibility']) if str(row['nqfAuthorityResponsibility']) != "nan" else ""
+        nqf_auth_resp = str(row['KKNI wewenang dan tanggung jawab']) if str(row['KKNI wewenang dan tanggung jawab']) != "nan" else ""
         description = str(row['Description']) if str(row['Description']) != "nan" else ""
-        ns_attitude = str(row['Attitude']) if str(row['Attitude']) != "nan" else ""
         domain = str(row['Domain']) if str(row['Domain']) != "nan" else ""
+        nqf_knowledge = str(row['KKNI pengetahuan']) if str(row['KKNI pengetahuan']) != "nan" else ""
+        nqf_working_skill = str(row['KKNI keterampilan kerja']) if str(row['KKNI keterampilan kerja']) != "nan" else ""
+        ns_attitude = str(row['SNDIKTI sikap']) if str(row['SNDIKTI sikap']) != "nan" else ""
+        ns_knowledge = str(row['SNDIKTI pengetahuan']) if str(row['SNDIKTI pengetahuan']) != "nan" else ""
+        ns_generic_skill = str(row['SNDIKTI keterampilan umum']) if str(row['SNDIKTI keterampilan umum']) != "nan" else ""
+        ns_specific_skill = str(row['SNDIKTI keterampilan khusus']) if str(row['SNDIKTI keterampilan khusus']) != "nan" else ""
 
-        if not all([curriculum, peo, nqf_auth_resp, description, ns_attitude, domain]):
+        if not all([curriculum, peo, description, domain]):
             error_list.append(index+2)
             continue
         
+        print("HALOGAN")
         curriculum = clean_cell(curriculum)
         peo = clean_cell(peo)
         domain = clean_cell(domain)
+        description = clean_cell(description)
+        nqf_auth_resp = clean_cell(nqf_auth_resp)
+        nqf_knowledge = clean_cell(nqf_knowledge)
+        nqf_working_skill = clean_cell(nqf_working_skill)
+        ns_attitude = clean_cell(ns_attitude)
+        ns_knowledge = clean_cell(ns_knowledge)
+        ns_generic_skill = clean_cell(ns_generic_skill)
+        ns_specific_skill = clean_cell(ns_specific_skill)
 
         query_string = f"""
             PREFIX : <{PREFIX}>
@@ -133,24 +151,31 @@ def import_curriculum_PEO(excel_file):
             INSERT {{
                 :{peo}  a :ProgramEducationalObjective ;
                         :label "{description}" ;
-                        :nqfAuthorityResponsibility "{nqf_auth_resp}" ;
-                        :nsAttitude "{ns_attitude}" ;
-                        :hasDomain :{domain} .
+                        :hasDomain :{domain} ;
+                        {f':nqfAuthorityResponsibility "{nqf_auth_resp}" ;'   if nqf_auth_resp != "" else ""}
+                        {f':nqfKnowledge "{nqf_knowledge}" ;'                 if nqf_knowledge != "" else ""}
+                        {f':nqfWorkingSkill "{nqf_working_skill}" ;'          if nqf_working_skill != "" else ""}
+                        {f':nsAttitude "{ns_attitude}" ;'                     if ns_attitude != "" else ""}
+                        {f':nsKnowledge "{ns_knowledge}" ;'                   if ns_knowledge != "" else ""}
+                        {f':nsGenericSkill "{ns_generic_skill}" ;'            if ns_generic_skill != "" else ""}
+                        {f':nsSpecificSkill "{ns_specific_skill}" ;'          if ns_specific_skill != "" else ""}
             }}
             WHERE {{
                 FILTER NOT EXISTS {{
                     :{peo} a :ProgramEducationalObjective .
                 }}
-            }};
-
+            }}; 
+            """
+        
+        query_string += f"""
             INSERT {{
-                :{curriculum} :hasPEO :{peo} .
-            }}
-            WHERE {{
-                FILTER NOT EXISTS {{
                     :{curriculum} :hasPEO :{peo} .
                 }}
-            }}
+                WHERE {{
+                    FILTER NOT EXISTS {{
+                        :{curriculum} :hasPEO :{peo} .
+                    }}
+                }}
             """
 
         sparql = SPARQLWrapper(GRAPHDB_POST) 
@@ -179,8 +204,18 @@ def import_PEO_PLO(excel_file):
         knowledge_cat = str(row['Knowledge Category']) if str(row['Knowledge Category']) != "nan" else ""
         course_code = str(row['Course Code']) if str(row['Course Code']) != "nan" else ""
         label = str(row['Description']) if str(row['Description']) != "nan" else ""
+        nqf_authority_responsibility = str(row['KKNI wewenang dan tanggung jawab']) if str(row['KKNI wewenang dan tanggung jawab']) != "nan" else ""
+        nqf_knowledge = str(row['KKNI pengetahuan']) if str(row['KKNI pengetahuan']) != "nan" else ""
+        nqf_working_skill = str(row['KKNI keterampilan kerja']) if str(row['KKNI keterampilan kerja']) != "nan" else ""
+        ns_attitude = str(row['SNDIKTI sikap']) if str(row['SNDIKTI sikap']) != "nan" else ""
+        ns_knowledge = str(row['SNDIKTI pengetahuan']) if str(row['SNDIKTI pengetahuan']) != "nan" else ""
+        ns_generic_skill = str(row['SNDIKTI keterampilan umum']) if str(row['SNDIKTI keterampilan umum']) != "nan" else ""
+        ns_specific_skill = str(row['SNDIKTI keterampilan khusus']) if str(row['SNDIKTI keterampilan khusus']) != "nan" else ""
 
-        if not all([peo, plo, sub_plo, rel, learning_domain, knowledge_cat, course_code, label]):
+        
+
+        if not all([peo, plo, sub_plo, rel, learning_domain, knowledge_cat, course_code, label, nqf_authority_responsibility, nqf_knowledge,
+                    nqf_working_skill, ns_attitude, ns_generic_skill, ns_specific_skill]):
             error_list.append(index+2)
             continue
         
@@ -208,14 +243,20 @@ def import_PEO_PLO(excel_file):
                 :{plo}  a :ProgramLearningOutcome ;
                         :partOf :{peo} ;
                         :label "{label}" ;
-                        :nsKnowledge "{knowledge_cat}" .
-                        :hasDomain :{learning_domain} ;
+                        :nqfAuthorityResponsibility "{nqf_authority_responsibility}";
+                        :nqfKnowledge "{nqf_knowledge}";
+                        :nqfWorkingSkill "{nqf_working_skill}" ;
+                        :nsAttitude "{ns_attitude}" ;
+                        :nsKnowledge "{knowledge_cat}" ;
+                        :nsGenericSkill "{ns_generic_skill}" ;
+                        :nsSpecificSkill "{ns_specific_skill}" ;
+                        :hasDomain :{learning_domain} .
             }}
             WHERE {{
                 FILTER NOT EXISTS {{
                     :{plo} a :ProgramLearningOutcome .
                 }}
-            }}
+            }};
             """
         
         if (sub_plo != "EMPTY"):
@@ -223,14 +264,14 @@ def import_PEO_PLO(excel_file):
             INSERT {{
                 :{sub_plo}  a :SubProgramLearningOutcome ;
                             :partOf :{plo} ;
-                            :nsKnowledge "{knowledge_cat}" .
+                            :nsKnowledge "{knowledge_cat}" ;
                             :hasDomain :{learning_domain} ;
             }}
             WHERE {{
                 FILTER NOT EXISTS {{
                     :{sub_plo} a :SubProgramLearningOutcome .
                 }}
-            }}
+            }};
             """
             if (rel == "yes"):
                 query_string += f"""
@@ -562,7 +603,7 @@ def import_course_PLO(excel_file):
     error_list = []
 
     # Transform data to dataframe pandas
-    df = pd.read_excel(excel_file,sheet_name='Course_CLO') # sheet_name = Sheet seq. number on excel/Sheet name
+    df = pd.read_excel(excel_file,sheet_name='Course_PLO') # sheet_name = Sheet seq. number on excel/Sheet name
 
     for index, row in df.iterrows():
         course = str(row['Course']) if str(row['Course']) != "nan" else ""
