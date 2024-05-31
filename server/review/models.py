578bb46d7cfc5577f2d2454a17a25e40c625cbd7
Arkan Alexei Andrei <arkan.alexei@ui.ac.id>
2024-05-31 19:17:58 +0700

    feat: add more fields to review model

--- server/review/models.py
+++ server/review/models.py
@@ -8,3 +8,12 @@ class Review(models.Model):
   comment = models.CharField(max_length=1000)
   rating = models.IntegerField()
   reviewer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+  query = models.CharField(max_length=1000)
+
+  class Meta:
+    constraints = [
+      models.UniqueConstraint(fields=['reviewer', 'query'], name='unique_reviewer_query')
+    ]
+  
+  def __str__(self):
+    return f'Review by {self.reviewer} for {self.query}'
