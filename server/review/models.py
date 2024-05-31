4a1ddd61a518665c3b81015aa29d28be70928ebd
Arkan Alexei Andrei <arkan.alexei@ui.ac.id>
2024-05-31 19:07:56 +0700

    feat: add review model

--- server/review/models.py
+++ server/review/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+class Review(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  comment = models.CharField(max_length=1000)
+  rating = models.IntegerField()
+  reviewer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
--- server/server/settings.py
+++ server/server/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "djoser",
     "drf_yasg",
     "users",
+    "review"
 ]
 
 MIDDLEWARE = [
