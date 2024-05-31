bbb3ccee5c1aa20d22bbce7875f326bbc07f40ea
Arkan Alexei Andrei <arkan.alexei@ui.ac.id>
2024-05-31 19:26:52 +0700

    feat: add curriculum

--- server/review/models.py
+++ server/review/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from users.models import CustomUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Review(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   comment = models.CharField(max_length=1000)
-  rating = models.IntegerField()
+  rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(4)])
   reviewer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
   query = models.CharField(max_length=1000)
+  curriculum = models.CharField(max_length= 1000)
 
   class Meta:
     constraints = [
@@ -16,4 +18,4 @@ class Review(models.Model):
     ]
   
   def __str__(self):
-    return f'Review by {self.reviewer} for {self.query}'
+    return f'Review by {self.reviewer} for {self.query} in curriculum {self.curriculum}'
