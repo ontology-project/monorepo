0887d7bd96a41f3d10b74fe75d655a7cb5eea7c7
James Smith Wigglesworth <jamesmithwigglesworth@gmail.com>
2024-06-11 23:23:28 +0700

    refactor: modify backend constants for deployment

--- server/utils/constants.py
+++ server/utils/constants.py
@@ -1,8 +1,8 @@
-GRAPHDB_GET="http://localhost:7200/repositories/coba"
+GRAPHDB_GET="http://localhost:7200/repositories/ontology-research-db"
 GRAPHDB_POST=GRAPHDB_GET + "/statements"
 
 
-PREFIX="http://www.semanticweb.org/arkan/ontologies/2024/2/untitled-ontology-18/"
+PREFIX="http://www.semanticweb.org/ami/ontologies/2024/0/OBE_Ont_V2/"
 OWL="http://www.w3.org/2002/07/owl#"
 RDF="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 OBE="http://www.semanticweb.org/ami/ontologies/2024/0/OBE#"
